ce8a8b5d5db4d43cbeed35a1f2af20b7af839b98
souravbiswas19 <souravbiswas19april@gmail.com>
2024-01-30 14:46:33 +0530

    FastAPI

--- crud.py
+++ crud.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app  = FastAPI()
 
-class Person:
+class Person(BaseModel):
     id: int
     firstname: str
     lastname:str
@@ -12,3 +13,16 @@ class Person:
 @app.get("/", status_code=200)
 def getPerson_Info():
     return {"message":"server is running"}
+
+@app.get('/getpersonbyid/{person_id}', status_code=200)
+def getPerson_by_Id(person_id: int):
+    return {"message": f'Your person id is {person_id}'}
+
+@app.post('/addpersoninfo', status_code=200)
+def addPerson_Info(person:Person):
+    return {
+        'id': person.id,
+        'firstname':person.firstname,
+        'lastname':person.lastname,
+        'isMale':person.ismale
+        }
