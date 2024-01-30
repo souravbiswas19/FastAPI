1eaa59b138c420090eeb72cc54565c514c54cfb5
souravbiswas19 <souravbiswas19april@gmail.com>
2024-01-30 12:53:53 +0530

    FastAPI

--- main.py
+++ main.py
@@ -20,12 +20,7 @@ def get_posts():
     return {"Data": "This is your post"}
 
 @app.post("/createposts")
-def get_posts(new_post: Post):
-    print(new_post.title)
-    print(new_post.content)
-    print(new_post.published)
-    print(new_post.rating)
-    print(new_post)
-    print(new_post.model_dump())
-    return {"data":"new post"}
+def get_posts(post: Post):
+    print(post.model_dump())
+    return {"data":post}
     #return {"new_post": f'title - {payload['title']} content - {payload['content']}'}
