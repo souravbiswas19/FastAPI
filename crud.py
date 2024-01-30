c65d0acb41a6d1bb6871a6688d6e1912a0c5f366
souravbiswas19 <souravbiswas19april@gmail.com>
2024-01-30 14:12:19 +0530

    FastAPI

--- crud.py
+++ crud.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app  = FastAPI()
 
+class Person:
+    id: int
+    firstname: str
+    lastname:str
+    ismale: bool
+
+
 @app.get("/", status_code=200)
-def getcar_Info():
+def getPerson_Info():
     return {"message":"server is running"}
